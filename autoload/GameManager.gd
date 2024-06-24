3895546a5f005e79661c5a7f8e0a495be7faf302
Flávio Silva <55815265+201flaviosilva@users.noreply.github.com>
2024-06-24 18:48:53 +0100

    Create ball extraction animation (#12)
    * fix: create a font theme for labels and buttons, and added card to MainMenu scene
    
    * feat: create ball extraction animation
    
    - Fixed: viewport problem;
    - Created: Border UI srpiet using paint;
    - Fixed: Card UI;
    - Fixed: Main scene UI;

--- autoload/GameManager.gd
+++ autoload/GameManager.gd
@@ -4,11 +4,23 @@ const NUMBER_OF_BALLS = 30
 const PLAYER_CARD_BALLS = 15
 
 const COLORS = {
-	RED = Color(1,0,0),
-	YELLOW = Color(1,1,0),
-	GREEN = Color(0,1,0),
-	AQUA = Color(0,1,1),
-	BLUE = Color(0,0,1),
-	WHITE = Color(1,1,1),
-	BLACK = Color(0,0,0),
+	RED = Color(1,0,0), # 1,10
+	YELLOW = Color(1,1,0), # 11, 20
+	GREEN = Color(0,1,0), # 21, 30
+	AQUA = Color(0,1,1), # 31, 40
+	BLUE = Color(0,0,1), # 41, 50
+	MAGENTA = Color(1,0,1), # 51, 60
+	WHITE = Color(1,1,1), # 61, 70
+	BLACK = Color(0,0,0), # 71, 80
+	GREY = Color(0.5,0.5,0.5), # 81, 90
+	BROWN = Color(0.62, 0.33, 0.17) # 91, 100
 }
+
+var balls = []
+
+func generate_balls() -> void:
+	balls = range(1, GameManager.NUMBER_OF_BALLS + 1)
+	balls.shuffle()
+
+func get_next_ball_number():
+	return balls.pop_front()
--- scenes/card/Card.gd
+++ scenes/card/Card.gd
@@ -2,12 +2,14 @@ extends Control
 
 @export var CardButton_Scene: PackedScene
 
-@onready var Grid: GridContainer = $GridContainer
+@onready var Grid: GridContainer = $NinePatchRect/VBoxContainer/GridContainer
 
 func _ready() -> void:
 	_create_buttons()
 
 func _create_buttons() -> void:
+	_remove_buttons()
+	
 	var values = Utils.generate_player_card()
 	
 	for i in range(GameManager.PLAYER_CARD_BALLS):
@@ -16,10 +18,22 @@ func _create_buttons() -> void:
 		
 		new_button.text = str(number)
 		new_button["theme_override_colors/font_color"] = Utils.calc_color(number)
-		new_button.button_down.connect(_on_button_pressed.bind(number, new_button))
+		new_button.pressed.connect(_on_card_button_pressed.bind(number, new_button))
 		
 		Grid.add_child(new_button)
 
-func _on_button_pressed(number: int, button: Button) -> void:
+func _remove_buttons() -> void:
+	var children = Grid.get_children()
+	
+	for child in children:
+		if not child is Label:
+			Grid.remove_child(child)
+			child.queue_free()
+
+func _on_card_button_pressed(number: int, button: Button) -> void:
 	button.disabled = true
 	print(number)
+
+func _on_bingo_button_pressed() -> void:
+	print("Bingo")
+	pass
