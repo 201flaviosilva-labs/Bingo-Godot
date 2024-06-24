4ab5a674869cecc211b912c9a6826cb987d756ef
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2024-06-25 00:20:04 +0100

    fix(#13): added a timer and play sound only on animation end

--- scenes/ball/Ball.gd
+++ scenes/ball/Ball.gd
@@ -2,20 +2,39 @@ extends Node2D
 
 class_name Ball
 
+signal animation_finish
+
 var ball_number: int = 0
 var ball_color: Color = Color(1, 1, 1)
 var tween: Tween
+var to_kill: bool = false
 
 func set_number(number: int) -> void:
 	ball_number = number
 	ball_color = Utils.calc_color(number)
 
-func start_move_animation(end_position: Vector2, start_position: Vector2 = position, time: float = 0.6) -> void:
+func extraction_animation(end_position: Vector2, start_position: Vector2 = position, time: float = 3) -> void:
 	position = start_position
 	tween = create_tween()
 	
 	tween.tween_property(self, "position", end_position, time)
+	tween.tween_property(self, "scale", Vector2(0.25,0.25), time / 4)
+	tween.finished.connect(_animation_finish)
+
+func move_side_animation(end_position: Vector2, time: float = 1) -> void:
+	tween = create_tween()
+	tween.tween_property(self, "position", end_position, time)
+	
+	if to_kill: # TODO: Fix
+		tween.parallel().tween_property(self, "scale", Vector2(0, 0), time)
+		tween.tween_callback(_kill)
 
 func _ready():
 	$Label.text = str(ball_number)
 	$Sprite2D.modulate = ball_color
+
+func _animation_finish() -> void:
+	animation_finish.emit()
+
+func _kill() -> void:
+	pass
--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -14,7 +14,7 @@ func _ready():
 	GameManager.generate_balls()
 
 func _new_ball() -> void:
-	var new_ball = Ball_Scene.instantiate()
+	var new_ball: Ball = Ball_Scene.instantiate()
 	var number = GameManager.get_next_ball_number()
 	
 	if not number: return; # TODO: Fix
@@ -24,24 +24,24 @@ func _new_ball() -> void:
 	
 	new_ball.set_number(number)
 	BallsWrapper.add_child(new_ball)
-	new_ball.start_move_animation(
-		Vector2(76 * children.size(), 0),
+	new_ball.extraction_animation(
+		Vector2(BALLS_MARGIN * children.size(), 0),
 		BALLS_START_POSITION,
-		1.5 - 0.1 * children.size()
+		GameManager.BALL_EXTRACTION_ANIMATION - 0.1 * children.size()
 		)
 	
-	_play_ball_sound(number)
+	new_ball.animation_finish.connect(_play_ball_sound.bind(number))
 
 func _move_children_ball() -> void:
-	var child = BallsWrapper.get_children()[0]
-	BallsWrapper.remove_child(child)
-	child.queue_free()
+	var kill_child = BallsWrapper.get_children()[0]
+	BallsWrapper.remove_child(kill_child) # TODO: Fix kill animation
+	kill_child.queue_free()
 	
 	for ball in BallsWrapper.get_children():
-		ball.start_move_animation(Vector2(ball.position.x - BALLS_MARGIN, 0))
+		ball.move_side_animation(Vector2(ball.position.x - BALLS_MARGIN, 0))
 
 func _play_ball_sound(number: int) -> void:
 	has_ball_asp.play() if Utils.player_has_number(number) else no_ball_asp.play()
 
-func _on_new_ball_button_pressed() -> void:
+func _on_ball_extraction_timer_timeout() -> void:
 	_new_ball()
