b6428230a926172e259ac11738901d7decdbc390
201flaviosilva <flavioandre.2001.9.7@gmail.com>
2024-06-25 17:13:48 +0100

    feat: created Initial Menu

--- scenes/main/Menu.gd
+++ scenes/main/Menu.gd
@@ -2,9 +2,12 @@ extends Control
 
 signal reset_click
 
+@onready var initial_menu: PanelContainer = $InitialMenu
 @onready var pause_menu: PanelContainer = $PauseMenu
-@onready var resume_button: Button = $PauseMenu/VBoxContainer/HBoxContainer/ResumeButton
-@onready var reset_button: Button = $PauseMenu/VBoxContainer/HBoxContainer/ResetButton
+@onready var resume_button: Button = $PauseMenu/Container/ButtonsContainer/ResumeButton
+@onready var reset_button: Button = $PauseMenu/Container/ButtonsContainer/ResetButton
+
+var is_game_stated: bool = false
 
 func _ready() -> void:
 	_reset()
@@ -14,13 +17,18 @@ func _input(event: InputEvent) -> void:
 		_toggle_pause()
 
 func _reset() -> void:
+	is_game_stated = false
 	get_tree().paused = true
+	pause_menu.hide()
+	initial_menu.show()
 	show()
 
 func _toggle_pause() -> void:
+	if not is_game_stated: return;
 	_resume() if get_tree().paused else _pause()
 
 func _resume() -> void:
+	is_game_stated = true
 	get_tree().paused = false
 	pause_menu.hide()
 	hide()
@@ -29,6 +37,10 @@ func _pause() -> void:
 	get_tree().paused = true
 	pause_menu.show()
 	show()
+
+func _on_play_button_pressed() -> void:
+	initial_menu.hide()
+	_resume()
 
 func _on_reset_button_pressed() -> void:
 	emit_signal("reset_click")
